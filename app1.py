e69e3a2c64b90d38e5389d829abbaee5e93e83b4
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-07-21 16:17:52 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -373,7 +373,7 @@ def main():
                        
                        
     
-if __name__=='__main__':
+if __name__=='__main__':                       
     main()
                                  
 
