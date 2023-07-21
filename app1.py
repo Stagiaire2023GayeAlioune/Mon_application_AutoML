c3f005fb7201f7178bb989d8c8df75cd64e6e806
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-07-21 16:22:46 +0200

    Update app1.py

--- app1.py
+++ app1.py
@@ -365,14 +365,12 @@ def main():
     #st.markdown(":blue[La partie Quatification. ] \n")    
     #st.markdown(La partie Quatification, unsafe_allow_html=False, *, help=None)     
     st.title(':blue[La partie Quatification. ] ')
-    quant=st.selectbox('Selectionner la methode de quatification utilisée: ', ["quatif_un_pol","quatif_double_pol"] 
+    #quant=st.selectbox('Selectionner la methode de quatification utilisée: ', ["quatif_un_pol","quatif_double_pol"] 
     #if quant=='quatif_un_pol':
          #### on met le code pour la partie quantification d'un polluant puis on affichedera les courbes obtenues , la valeurs de la concentration du polluant obtenue 
          ## la fonction d'ajustement , y_intercept , x_intercept , delta_x ,    
                        
                        
-                       
-    
 if __name__=='__main__':                       
     main()
                                  
