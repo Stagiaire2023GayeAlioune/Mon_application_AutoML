fedb752c31d5699cc5db91c0ca2719aa9d29d4df
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-10-29 19:01:03 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -6,26 +6,7 @@ import pandas as pd
 
 import pandas_profiling
 
-
 from streamlit_pandas_profiling import st_profile_report
-
-from pycaret.regression import setup as setup_reg
-
-from pycaret.regression import compare_models as compare_models_reg
-
-from pycaret.regression import save_model as save_model_reg
-
-from pycaret.regression import plot_model as plot_model_reg
-
-from pycaret.classification import setup as setup_class
-
-from pycaret.classification import compare_models as compare_models_class
-
-from pycaret.classification import save_model as save_model_class
-
-from pycaret.classification import plot_model as plot_model_class
-
-import mlflow
 
 
 
