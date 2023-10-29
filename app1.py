c4206f07af828c26b710a2eb7f4c9831cb953f1a
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2023-10-29 18:56:11 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,9 +1,33 @@
 import streamlit as st
-from PIL import Image, ImageOps
-import ydata_profiling 
+
 import numpy as np
-from streamlit_pandas_profiling import st_profile_report
+
 import pandas as pd
+
+import pandas_profiling
+
+
+from streamlit_pandas_profiling import st_profile_report
+
+from pycaret.regression import setup as setup_reg
+
+from pycaret.regression import compare_models as compare_models_reg
+
+from pycaret.regression import save_model as save_model_reg
+
+from pycaret.regression import plot_model as plot_model_reg
+
+from pycaret.classification import setup as setup_class
+
+from pycaret.classification import compare_models as compare_models_class
+
+from pycaret.classification import save_model as save_model_class
+
+from pycaret.classification import plot_model as plot_model_class
+
+import mlflow
+
+
 
 @st.cache_data
 
