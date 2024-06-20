d3a0cf2267e4addaa63cfacc35da1c4e3f09d771
Gaye_Alioune <131458147+Stagiaire2023GayeAlioune@users.noreply.github.com>
2024-06-20 03:25:04 +0200

    Update reserv.py

--- reserv.py
+++ reserv.py
@@ -139,7 +139,7 @@ def generate_pdf(reservations):
 
 # Sélection de la page
 page = st.sidebar.selectbox("Choisissez le type de reservation", ["Commande à Emporter", "Commande sur Place"])
-st.sidebar.image("https://th.bing.com/th/id/OIP.yP8OOsRFeygNvQFJr7SYJQHaB5?w=328&h=89&c=7&r=0&o=5&dpr=1.5&pid=1.7", use_column_width=True)
+st.sidebar.image("casa.png", use_column_width=True)
 
 if page == "Commande à Emporter":
     page_emporter()
